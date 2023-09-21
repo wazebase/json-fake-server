730761b28314292d2c01b113be00a946cee9957a
wazebase <50338875+wazebase@users.noreply.github.com>
2023-09-21 22:27:28 +0300

    Update server.js

--- json-server-vercel/api/server.js
+++ json-server-vercel/api/server.js
@@ -1,18 +1,24 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
 
-server.use(middlewares)
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/parcel': '/contact',
-}))
-server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+ console.log("JSON Server is running");
+});
 
 // Export the Server API
 module.exports = server
